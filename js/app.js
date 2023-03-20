4cdf9f5fb4467f001e5d1aa384a0a33791e7c6aa
HectorHTL <113003064+HectorHTL@users.noreply.github.com>
2023-03-19 21:25:45 -0600

    fix upload image

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // }
 
 import { getAllUsersFire, createUserFire,deleteUserFire,createObservationFire } from "/users/js/firebase-functions.js";
-import {getStorage,ref, uploadBytes,getDownloadURL } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-storage.js";
+import {getStorage,ref, uploadBytes,getDownloadURL, uploadBytesResumable} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-storage.js";
 import { app } from "/users/js/firebase.js";
 const storage = getStorage(app);
 const getAllUsers = () => {
@@ -18,20 +18,39 @@ const textModal = document.getElementById("textModal");
 
 
 
+
 const saveUser = async () => {
-  
-  const file = document.querySelector("#image").files[0];
+
+
+ const file = document.querySelector("#image").files[0];
   const name = file.name;
-  const storageRef  = ref(storage, "images/"+name);
-  var img = "";
-  const metadata = {
-    contentType : file.type
-  }
-  const task = uploadBytes(storageRef, file, metadata);
+ const storageRef  = ref(storage, "images/"+name);
+
+  
+const uploadTask = uploadBytesResumable(storageRef, file);
+
+
+uploadTask.on('state_changed',
+  (snapshot) => {
  
-  getDownloadURL(storageRef)
-  .then(async (url) =>  {
-    console.log("Entra crear usuario")
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+      case 'running':
+        console.log('Upload is running');
+        break;
+    }
+  },
+  (error) => {
+  },
+  () => {
+
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      console.log('File available at', downloadURL);
+      console.log("Entra crear usuario")
   const user = {
     name: "",
     lastName : "",
@@ -53,10 +72,8 @@ const saveUser = async () => {
   const state = document.getElementById("stateNew");
   const phone = document.getElementById("phoneNew");
   const mail = document.getElementById("mailNew");
+  var  img=downloadURL;
   
-  img=url;
-
-  console.log(img);
   user.name = txtName.value;
   user.lastName = lastName.value,
   user.position = position.value,
@@ -68,7 +85,7 @@ const saveUser = async () => {
   user.image = img
 
   
-  const generatedId = await createUserFire(user);
+  const generatedId =  createUserFire(user);
   if (generatedId != "no-created") {
     txtName.value = "";
     Toastify({
@@ -85,33 +102,13 @@ const saveUser = async () => {
     alert("Usuario no creado");
   }
 
-  })
-  .catch((error) => {
-    // A full list of error codes is available at
-    // https://firebase.google.com/docs/storage/web/handle-errors
-    switch (error.code) {
-      case 'storage/object-not-found':
-        // File doesn't exist
-        break;
-      case 'storage/unauthorized':
-        // User doesn't have permission to access the object
-        break;
-      case 'storage/canceled':
-        // User canceled the upload
-        break;
-
-      // ...
-
-      case 'storage/unknown':
-        // Unknown error occurred, inspect the server response
-        break;
-    }
-  });
-
-  
+    });
+  }
+);
 
 
 setTimeout(() => {location.reload() }, 3000);
+
 
 };
 
